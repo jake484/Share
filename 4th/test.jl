8b01716e7d4a38951100a3ad3dac76d0828a16b0
yjy <522432938@qq.com>
2024-01-25 22:02:10 +0800

    update 4th

--- 4th/test.jl
+++ 4th/test.jl
@@ -1,40 +1,26 @@
 include("components.jl")
+
 using Test
+
 @named resistor = Resistor(R=3.0)
 @named capacitor = Capacitor(C=2.0)
 @named source = ConstantVoltage(V=1.0)
 @named ground = Ground()
+
 rc_eqs = [
     connect(source.p, resistor.p)
     connect(resistor.n, capacitor.p)
     connect(capacitor.n, source.n, ground.g)
 ]
+
 @named _rc_model = ODESystem(rc_eqs, t)
 @named rc_model = compose(_rc_model, [resistor, capacitor, source, ground])
 sys = structural_simplify(rc_model)
+
 equations(sys) |> display
 prob = ODEProblem(sys, [capacitor.v => 0.0], (0, 10.0))
 sol_mtk = solve(prob, Tsit5(), saveat=0.1)
 @test sol.retcode == ReturnCode.Success
-# 显示所有状态
-# for state in states(rc_model)
-#     println(state, " = ", sol[state][1:2])
-# end
-
-# 求解步长设置
-
-# 组合元件，化简
-@named r = Resistor(R=1.0)
-@named r2 = Resistor(R=2.0)
-@named c = Capacitor(C=1.0)
-con = [
-    connect(r.p, c.p)
-    connect(r2.p, c.n)
-    connect(r.n, r2.n)
-]
-@named _model = ODESystem(rc_eqs, t)
-@named model = compose(_model, [resistor, capacitor, source, ground])
-sys = structural_simplify(model)
 
 # 生成函数语法树
 ModelingToolkit.generate_function(sys)
@@ -42,7 +28,6 @@ ModelingToolkit.generate_function(sys)
 open("func.jl", "w") do io
     write(io, string(ModelingToolkit.generate_function(sys)[2]))
 end
-
 
 # ModelingToolkit.varmap_to_vars(
 #     [
